ea9483e1f9bdfdecc93abbe57f4ffd5037f0dfdd
Raissa <raissacaitlinp@gmail.com>
2019-06-26 13:00:02 -0400

    added the zap interface library

---
+++ contracts/mainmarket/MainMarket.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.8;
+
+contract MainMarket {
+
+    mapping (address => uint) holders;
+
+
+
+    constructor() public {
+
+    }
+
+
+    //Disperse 5% fees to all
+    function payFee() private {
+
+    }
+}
---
+++ contracts/mainmarket/MainMarketInterface.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.8;
+
+contract MainMarketInterface {
+
+//    //Withdraw 5% to holders
+//    function withdraw() external;
+//
+//    //Convert the asset tokens to Zap Tokens
+//    function exchangeZap() private;
+//
+//    //Disperse funds to holders 5% fee based on percentage of stake
+//    function disperseFunds() private;
+//
+//    //Self destruct the contract if it goes bankrupt
+//    function selfDestruct() external;
+//
+//    //Deposits Zap Tokens to MainMarket Contract
+//    function deposit() payable external;
+//
+//    //Get current Zap price
+//    function getZapPrice() external;
+
+}
